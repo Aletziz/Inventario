b857b971bdf4ecde2a06d0f9116572da77cda415
Aletziz <galexito856@gmail.com>
2025-05-05 23:44:59 -0400

    Add files via upload

--- build.sh
+++ build.sh
@@ -4,4 +4,15 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python -c "from app import app, db; app.app_context().push(); db.create_all()"
+# Inicializar la base de datos solo si no existe
+python -c "
+from app import app, db
+with app.app_context():
+    # Verificar si las tablas existen
+    inspector = db.inspect(db.engine)
+    if not inspector.has_table('producto'):
+        print('Creando tablas de la base de datos...')
+        db.create_all()
+    else:
+        print('Las tablas ya existen, no se realizará ninguna modificación.')
+"
